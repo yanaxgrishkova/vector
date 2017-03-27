9c260fe50ffaff9e7269d271ae155525d74fffb4
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-03-27 13:13:24 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -15,6 +15,6 @@ public:
     auto size() const noexcept -> unsigned int;
     auto capacity() const noexcept -> unsigned int;
     auto ptr() const noexcept -> unsigned int
-    auto push_back(int value) -> void;
+ 	auto push_back(int value) -> void;
     auto operator[](unsigned int index) const noexcept -> int;
 };
