c37d297785b51e5af6063dbeeec98595c45905ae
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-03-27 13:06:53 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,27 @@
 #include <vector.hpp>
 #include <catch.hpp>
 
-SCENARIO("vector init", "[init]") {
+SCENARIO("default constructor") {
 	vector_t vector;
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
+}
+
+SCENARIO("constructor with params") {
+	vector_t vector(1, 2);
+	REQUIRE(vector.size() == 1);
+	REQUIRE(vector.capacity() == 2);
+}
+
+SCENARIO("copy constructor") {
+	vector_t vector(1, 2);
+	vector_t copy(vector);
+	REQUIRE(copy.size_() == 1);
+	REQUIRE(copy.capacity_() == 2);
+}
+
+SCENARIO("operator =") {
+	complex_t v1(7, 6); 
+	complex_t v2=v1;
+	REQUIRE(v2==v1);
 }
